97e05c58797c0840e05b5379d5ede78ccbd2c267
JordanR314 <jordanlamb314@gmail.com>
2026-01-22 15:03:39 -0500

    Add files via upload

---
+++ First.java
@@ -0,0 +1,8 @@
+void main() {
+    // print statement for greeting and job title, uses \n for different line
+    System.out.println("Hola mundo!\nJob title: Electrical and Electronics Engineer");
+    // print statement for reasoning
+    System.out.println("Why: I have always been intrigued by electronics");
+    // print statement for median salary
+    System.out.println("Median salary: $118,780");
+}
