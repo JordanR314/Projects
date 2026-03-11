f8050a2433c68cf1ab5e4b28e1a25be7caf3beba
JordanR314 <jordanlamb314@gmail.com>
2026-03-11 15:20:18 -0400

    Add files via upload

--- Calc.java
+++ Calc.java
@@ -1,58 +1,62 @@
 // Sets up Calc class for runner
 public class Calc {
 
-// Creates private variable data fields
+    // Creates private variable data fields
     private double num1;
     private double num2;
 
-// Sets default values for Calc class
+    // Sets default values for Calc class
     public Calc() {
         this.setNum1(0);
         this.setNum2(0);
     }
 
-// setNum method for num1, set to var newNum1
+    // setNum method for num1, set to var newNum1
     public void setNum1(double newNum1) {
         this.num1 = newNum1;
     }
 
-// getNum method for num1, returns value
+    // getNum method for num1, returns value
     public double getNum1() {
         return this.num1;
     }
 
-// setNum method for num2, set to var newNum2
+    // setNum method for num2, set to var newNum2
     public void setNum2(double newNum2) {
         this.num2 = newNum2;
     }
 
-// getNum method for num2, returns value
+    // getNum method for num2, returns value
     public double getNum2() {
         return this.num2;
     }
 
-// add method, returning the sum
-    public double add(){
-        return num1 + num2;
+    // add method, returning the sum
+    public double add() {
+        double sum = Math.round((num1 + num2) * 100.0) / 100.0;
+        return sum;
     }
 
-// subtract method, returning difference
-    public double subtract(){
-        return num1 - num2;
+    // subtract method, returning difference
+    public double subtract() {
+        double sub = Math.round((num1-num2) * 100.0) / 100.0;
+        return sub;
     }
 
-// multiply method, returning product
-    public double multiply(){
-        return num1 * num2;
+    // multiply method, returning product
+    public double multiply() {
+        double prod = Math.round((num1*num2) * 100.0) / 100.0;
+        return prod;
     }
 
-// divide method, returning quotient
-    public double divide(){
-        return num1/num2;
+    // divide method, returning quotient
+    public double divide() {
+        double quo = Math.round((num1/num2) * 100.00) / 100.00;
+        return quo;
     }
 
-// toString method, return formatted output
-    public String toString(){
+    // toString method, return formatted output
+    public String toString() {
         String output = "Num1: " + num1;
         output += "\nNum2: " + num2;
         return output;
--- Runner.java
+++ Runner.java
@@ -1,9 +1,69 @@
-public class Runner { // Sets up runner class for main
+//this class tests all methods in the Calc class
+//the Calc class exposes the following methods:
+//add, subtract, multiply, divide
+//user is prompted for input
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Runner {
+    public static void main(String[] args){
+        //instantiate a Calc object
+        Calc myCalculator = new Calc();
+        //get user input for two numbers
+        Scanner scan = new Scanner(System.in);
 
-    public static void main (String[]args) { // main for blueprint call
+        // Setup for n1 value and boolean, input checking
+        double n1 = 0.0;
+        boolean corr = false;
 
-        Tests tester = new Tests(); // Initiates tester, calls getAverage, and print with toString method
-        tester.getAverage();
-        System.out.println(tester);
+        while (!corr) { // While loop to repeatedly check input
+            System.out.println("Please enter the first number: ");
+
+            if (scan.hasNextDouble()) { // Checks if the input is a double
+                n1 = scan.nextDouble();
+                corr = true; // Input is valid, exit the loop
+            }
+            else { // Input is invalid
+                System.out.println("Please enter a valid input: ");
+                scan.next(); // Takes the invalid input
+            }
+        }
+
+        // Setup for n2 value and boolean, input checking
+        double n2 = 0.0;
+        boolean corr1 = false;
+
+        while (!corr1) { // While loop to repeatedly check input
+            System.out.println("Please enter the second number: ");
+
+            if (scan.hasNextDouble()) { // Checks if the input is a double
+                n2 = scan.nextDouble();
+                corr1 = true; // Input is correct
+            }
+            else { // Input is invalid
+                System.out.println("Please enter a valid input: ");
+                scan.next(); // Takes the invalid input
+            }
+        }
+
+        //pass the numbers to the Calc object
+        myCalculator.setNum1(n1);
+        myCalculator.setNum2(n2);
+
+        //output from Calc instance
+        System.out.println(myCalculator);
+
+        //examining the instance private data fields by calling get methods
+        System.out.println("Calling num1 get method: " + myCalculator.getNum1());
+        System.out.println("Calling num2 get method: " + myCalculator.getNum2());
+
+        //calling Calc methods directly
+        //can use a local variable for sum if you need that value for another calculation
+        double sum = myCalculator.add();
+        System.out.println("The sum is: " + sum);
+
+        //or if you don't, then just display the difference, product and quotient
+        System.out.println("The difference is: " + myCalculator.subtract());
+        System.out.println("The product is: " + myCalculator.multiply());
+        System.out.println("The quotient is: " + myCalculator.divide());
     }
 }
