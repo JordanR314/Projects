aac1bd012b1a69b6bac6ae493221d94a06eb8828
JordanR314 <jordanlamb314@gmail.com>
2026-04-01 22:19:31 -0400

    Add files via upload

--- Asn5.java
+++ Asn5.java
@@ -38,32 +38,39 @@ public class Asn5 {
             acc++;
         }
 
-        //Creates the new
+        //Instantiates the new Integer Array
         ArrayList<Integer> arr2 = new ArrayList<>();
 
+        //Uses the add method to create an Integer Array with size 4
         arr2.add(1);
         arr2.add(2);
         arr2.add(3);
         arr2.add(4);
 
+        //Prints the title for the Integer Array's contents
         System.out.println("\nArrayList 2 contents");
 
+        //Iterates through each index and prints the formatted list
         acc = 0;
         for (Integer s : arr2) {
             System.out.println("Index " + acc + ": " + s);
             acc++;
         }
 
+        //Uses the set method to set the value at index 2 to integer value 8
         arr2.set(2, 8);
 
+        //Prints the description of the set method on the original Integer Array
         System.out.println("\nArrayList 2 contents after 'set method' for Integer 8 at index 2");
 
+        //Iterates through each index and prints the formatted list
         acc = 0;
         for (Integer s : arr2) {
             System.out.println("Index " + acc + ": " + s);
             acc++;
         }
 
+        //Prints the description for using the size method and formats the output
         System.out.println("\nResult of 'size method' for ArrayList 2");
         System.out.println("The size of ArrayList 2 is: " + arr2.size());
 
