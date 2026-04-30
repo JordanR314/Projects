fd06dfbc718bb63d30cf5f83d968bc4bfdb103de
JordanR314 <jordanlamb314@gmail.com>
2026-04-30 17:48:44 -0400

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -1,49 +1,7 @@
-import javax.swing.JOptionPane;
-/**
- This program demonstrates the Exam class,
- which extends the Assessment class.
- */
-
 public class Runner {
-
-        public static void main(String[] args)
-        {
-            //try block to check the inputs as integers and the questions as not 0
-            try {
-                String input;     // To hold input
-                int questions;    // Number of questions
-                int missed;       // Number of questions missed
-
-                // Get the number of questions on the exam
-                input = JOptionPane.showInputDialog("How many questions are on the exam?");
-                questions = Integer.parseInt(input);
-
-                // Get the number of questions the student missed
-                input = JOptionPane.showInputDialog("How many questions did the student miss?");
-                missed = Integer.parseInt(input);
-
-                // Create an Exam object
-                Exam exam = new Exam(questions, missed);
-
-                // Display the test results
-                String message = "Each question counts " + exam.getPointsEach();
-                message += " points.\nThe exam score is " + exam.getScore();
-                message += "\nThe exam grade is " + exam.getGrade();
-                JOptionPane.showMessageDialog(null, message);
-
-                System.exit(0);
-            }
-            // Catches the exception for input not being an integer
-            catch (NumberFormatException e){
-                System.out.println("NumberFormatException: " + e.getMessage());
-                System.exit(0);
-            }
-            // Catches the exception for when the questions are 0 and the questions missed are 0
-            catch (ArithmeticException e){
-                System.out.println("ArithmeticException: " + e.getMessage());
-                System.exit(0);
-            }
-        }
-    }
-
-
+    //Instantiate a Towers object to swap the rings on 3 "towers"
+    public static void main(String[] args)
+     {
+            Towers towersOfHanoi = new Towers(3);
+       }
+}
